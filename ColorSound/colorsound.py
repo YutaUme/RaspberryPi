8ffb4670c08c43e28b510048f7e3bb0d9936c377
yutaume <yutaume.0927@gmail.com>
2018-02-22 22:22:22 +0900

    色検知追加

--- ColorSound/colorsound.py
+++ ColorSound/colorsound.py
@@ -13,12 +13,122 @@ def capture():
     #time.sleep(2) #カメラ初期化
     camera.capture('foo.jpg')
 
+def sound(hue,p):
+	print(hue)
+	if(hue == 1):
+		print("C")
+		p.ChangeFrequency(262)
+		p.start(100)
+		p.stop()
+	elif(hue == 2):
+		print("C#")
+		p.ChangeFrequency(277)
+		p.start(100)
+		p.stop()
+	elif(hue == 3):
+		print("D")
+		p.ChangeFrequency(294)
+		p.start(100)
+		p.stop()
+	elif(hue == 4):
+		print("D#")
+		p.ChangeFrequency(311)
+		p.start(100)
+		p.stop()
+	elif(hue == 5):
+		print("E")
+		p.ChangeFrequency(330)
+		p.start(100)
+		p.stop()
+	elif(hue == 6):
+		print("F")
+		p.ChangeFrequency(349)
+		p.start(100)
+		p.stop()
+	elif(hue == 7):
+		print("F#")
+		p.ChangeFrequency(370)
+		p.start(100)
+		p.stop()
+	elif(hue == 8):
+		print("G")
+		p.ChangeFrequency(392)
+		p.start(100)
+		p.stop()
+	elif(hue == 9):
+		print("G#")
+		p.ChangeFrequency(415)
+		p.start(100)
+		p.stop()
+	elif(hue == 10):
+		print("A")
+		p.ChangeFrequency(440)
+		p.start(100)
+		p.stop()
+	elif(hue == 11):
+		print("A#")
+		p.ChangeFrequency(466)
+		p.start(100)
+		p.stop()
+	elif(hue == 12):
+		print("B")
+		p.ChangeFrequency(493)
+		p.start(100)
+		p.stop()
 
+
+
+
+
+## Main
 
 t = 0
+
+SOUNDER = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SOUNDER,GPIO.OUT,initial=GPIO.LOW)
+
+p = GPIO.PWM(SOUNDER,1)
+
 while(1) :
+	
+	#撮影
 	capture()
+
+	# 画像を読み込み
+	img_scr = cv2.imread("foo.jpg")
+
+	# BGR->HSV に変換
+	img_hsv = cv2.cvtColor(img_scr, cv2.COLOR_BGR2HSV)
+
+	height = img_hsv.shape[0]
+	width = img_hsv.shape[1]
+
+	# ピクセル抽出用の配列
+	pixels = []
+
+	# ある閾値を見たすピクセルと抽出
+	for y in range(height):
+		for x in range(width):
+			if (img_hsv[y, x, 1] > 45 and 32 < img_hsv[y, x, 2]):
+				pixels.append(img_hsv[y, x, 0])
+
+	# Hue判定用に180を15ずつの区画に区切る
+	bins = np.linspace(0, 180, 13)
+
+	index = np.digitize(pixels,bins)
+	
+	c = Counter(index)
+
+	# 最頻値
+	mode = c.most_common(1)
+
+	sound(mode[0][0],p)
+
 	print(t)
 	time.sleep(1)
 	t = t + 1
+
+
+
 
