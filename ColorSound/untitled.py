19b2e361cf141a64e666fc2ad554dfb4f516377e
yutaume <yutaume.0927@gmail.com>
2018-03-21 19:46:57 +0900

    修正

--- ColorSound/untitled.py
+++ ColorSound/untitled.py
@@ -7,29 +7,32 @@ import RPi.GPIO as GPIO
 import picamera
 import sys
 
-
+import pigpio
 
 
 SOUNDER = 18 # GPIO番号
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SOUNDER,GPIO.OUT)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(SOUNDER,GPIO.OUT)
 
-p = GPIO.PWM(18,int(sys.argv[1]))
+pin = pigpio.pi()
+pin.set_mode(SOUNDER, pigpio.OUTPUT)
+pin.hardware_PWM(SOUNDER, 0, 0)
 
-print(p)
-p.start(100)
+# p = GPIO.PWM(18,int(sys.argv[1]))
 
-p.ChangeFrequency(200)
 
-time.sleep(2)
-
-print(p)
-p.ChangeFrequency(800)
+pin.hardware_PWM(SOUNDER, 4000, 500000)
 
 time.sleep(2)
 
-print(p)
-p.ChangeFrequency(440)
+pin.hardware_PWM(SOUNDER, 2000, 500000)
 
-p.stop()
+time.sleep(2)
+
+pin.hardware_PWM(SOUNDER, 2000, 500000)
+
+
+pin.hardware_PWM(SOUNDER, 0, 0)
+pin.set_mode(SOUNDER, pigpio.INPUT)
+pin.stop()
 GPIO.cleanup()
